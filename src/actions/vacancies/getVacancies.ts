e8c060c9ba5645ad6b0e8717c2207b6f4f62fa91
artem-yaremchuk <artem.yaremchuk@yahoo.com>
2026-03-22 18:34:36 +0200

    feat(vacancy): add getVacancies action and vacancyService, add logger, update vacancy table, adjust SQL seed

---
+++ src/actions/vacancies/getVacancies.ts
@@ -0,0 +1,17 @@
+"use server";
+
+import { getLocale } from "next-intl/server";
+import { vacancyService } from "@/lib/vacancies/vacancy.service";
+import { VacancyType } from "@/types/vacancy";
+import { Locale } from "@/types/locale";
+
+export async function getVacancies(type?: VacancyType, page?: number, limit?: number) {
+  const locale = (await getLocale()) as Locale;
+
+  return vacancyService.getAll({
+    locale,
+    type,
+    page,
+    limit,
+  });
+}
