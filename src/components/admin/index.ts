406b3c2e68df52984c6f77ec27fa31fd3b69e9b8
Nataliia-Korneiko <53409659+Nataliia-Korneiko@users.noreply.github.com>
2026-04-29 23:41:38 +0300

    feat: add admin view site link

--- src/components/admin/index.ts
+++ src/components/admin/index.ts
@@ -13,3 +13,5 @@ export { Contract1824Section } from "./contract-18-24-section";
 export { SubdivisionSection } from "./subdivisions-section";
 
 export { SubdivisionsListSection } from "./subdivisions-section/subdivisions-list-section";
+
+export { AdminViewSiteButton } from "./admin-view-site-button";
