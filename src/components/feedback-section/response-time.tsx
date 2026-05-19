b55a3798903feb8c84e7885683d54e5e8c2b24ad
Dmytro <davudyuck0501@gmail.com>
2026-05-19 13:52:32 +0300

    feat: enhance feedback form and modal responsiveness, update placeholders, and improve styling across components

--- src/components/feedback-section/response-time.tsx
+++ src/components/feedback-section/response-time.tsx
@@ -5,9 +5,9 @@ interface ResponseTimeProps {
 
 export const ResponseTime = ({ title, description }: ResponseTimeProps) => {
   return (
-    <div className="border-dark-gray w-auto rounded-xs border px-10 py-10">
+    <div className="border-dark-gray desktop-xl:py-14 tablet:py-10 tablet:px-6 desktop:p-10 w-auto rounded-xs border px-4 py-7">
       <h3 className="font-ermilov text-soft-blush mb-3 text-[20px] uppercase">{title}</h3>
-      <p className="text-14px text-soft-blush">{description}</p>
+      <p className="text-soft-blush text-[14px] font-medium">{description}</p>
     </div>
   );
 };
--- src/components/modal/style.tsx
+++ src/components/modal/style.tsx
@@ -7,7 +7,7 @@ interface IGetStyles {
 
 export const getStyles = ({ isOpen, classNameOverlay }: IGetStyles) => ({
   overlay: cn(
-    "fixed top-0 left-0 z-[998] flex h-screen w-screen items-center justify-center bg-black/40",
+    "fixed inset-0 overflow-y-auto top-0 left-0 z-[998] flex h-screen w-screen items-center justify-center bg-black/40",
     classNameOverlay,
     {
       "animate-fade-in-opacity": isOpen,
