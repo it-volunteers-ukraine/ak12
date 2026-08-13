697d42f10cf9cddb94783fa9de072430fcc68ae7
Vlad <vladkostirya4@gmail.com>
2026-08-13 20:43:56 +0300

    fix padding

--- src/app/[locale]/management-console-12ak/careers/list/page.tsx
+++ src/app/[locale]/management-console-12ak/careers/list/page.tsx
@@ -1,12 +1,11 @@
-
 import { getAllVacanciesAdmin } from "@/actions/vacancies/get-all-vacancies-admin.action";
 import { VacanciesListSection } from "@/components/admin/vacancies-section/vacancies-list-section";
- 
+
 export default async function VacanciesListPage() {
   const { uk, en } = await getAllVacanciesAdmin();
 
   return (
-    <div className="px-4 py-6">
+    <div className="py-6 pr-4">
       <VacanciesListSection vacanciesUk={uk} vacanciesEn={en} />
     </div>
   );
--- src/lib/form-builder/components/FormBuilder.tsx
+++ src/lib/form-builder/components/FormBuilder.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { FieldValues, useFormContext } from "react-hook-form";
+
 import { cn } from "@/utils";
 import { FormImg } from "@/components/form-elements";
 import { BtnGroup } from "@/components/admin/admin-form-elements";
+
 import { FormBuilderConfig } from "../types";
 import { LocaleSection } from "./LocaleSection";
 
@@ -164,7 +166,7 @@ export const FormBuilder = ({
     <div className={config.className || "form-builder"}>
       <div
         className={cn(
-          `sticky top-22 z-30 mb-6 border-b border-gray-200 bg-white/95 px-2 backdrop-blur supports-backdrop-filter:bg-white/80 ${
+          `sticky top-22 z-30 mb-6 border-b border-gray-200 bg-white/95 backdrop-blur supports-backdrop-filter:bg-white/80 ${
             config.buttonsClassName || ""
           }`,
         )}
